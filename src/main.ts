a3acc0dbeb7333a834ecc44cf99715cd653a7378
Ian Zamojc <izamojc@zamtools.com>
2022-01-08 05:30:58 -0500

    gtag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag-next'
 import '@/assets/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueGtag, {
+  isEnabled: process.env.NODE_ENV !== 'development',
+  property: {
+    // id: 'G-01GZH4SWQZ', // local
+    id: 'G-G6STRX97S7', // prod
+  },
+})
+
+app.mount('#app')
